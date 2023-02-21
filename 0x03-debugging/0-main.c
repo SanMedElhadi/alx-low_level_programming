a14975f5d2ebf629fef94ccc70141bf652922056
SanMedElhadi <fm_SANNEF@esi.dz>
2023-02-21 05:59:33 -0800

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-main.c
+ * Author: Mohammed Elhadi SANNEF <fm_SANNEF@esi.dz>
+ */
+#include "main.h"
+
+/**
+ * main - Test positive_negative function when 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+In this repo, we test and debug some functions.
