8b6ac9f51d49d1be95407966b33abfa1cff9c44f
SanMedElhadi <fm_SANNEF@esi.dz>
2023-02-23 02:28:10 -0800

    Checked betty style for uppecase file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,13 +5,14 @@
 #include "main.h"
 
 /**
- * _isupper - Check if an alphabet is uppercase or not
+ * _isupper - Check if an alphabet is uppercase or not.
+ * @c : the caracter to check.
  *
- * Return: 1 When Uppercase, 0 elsewhere
+ * Return: 1 When Uppercase, 0 elsewhere.
  */
 int _isupper(int c)
 {
-	if ( c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	return (0);
 }
