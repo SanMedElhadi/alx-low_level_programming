cd4ab188d967b6227f8054b9f6af9b2a7b6d51c0
SanMedElhadi <fm_SANNEF@esi.dz>
2023-02-23 02:25:38 -0800

    Added uppercase function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+/*
+ * File : 0-isupper.c
+ * Author : Mohammed Elhadi SANNEF <fm_SANNEF@esi.dz>
+ */
+#include "main.h"
+
+/**
+ * _isupper - Check if an alphabet is uppercase or not
+ *
+ * Return: 1 When Uppercase, 0 elsewhere
+ */
+int _isupper(int c)
+{
+	if ( c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+In this repo, we continue by adding some extra functions that covers c programming, basic stuff with some nasted loops.
