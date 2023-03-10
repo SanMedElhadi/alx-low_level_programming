3c547c7e106bbf6edce8457cfbe8b9ce9d5cb446
SanMedElhadi <fm_SANNEF@esi.dz>
2023-03-10 08:49:09 -0800

    Corrected putchar function

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -5,20 +5,11 @@
 #include "main.h"
 
 /**
- * main - Prints _putchar.
+ * _putchar - Prints _putchar.
  *
  * Return: Always 0
  */
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
 	return (0);
 }
